294acbdd90ee71460f42b44118ac47cd6fa49607
Joe Marron <marronjo@joes-air.home>
2023-08-13 10:29:12 +0100

    Disable security config

--- src/main/java/com/marronjo/crypto/CryptoApplication.java
+++ src/main/java/com/marronjo/crypto/CryptoApplication.java
@@ -3,8 +3,9 @@ package com.marronjo.crypto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CryptoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoApplication.class, args);
